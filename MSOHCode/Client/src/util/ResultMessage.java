a3f1f4f4e1fdbd96c6fdb4b9e3ae18dca064ad99
Cflelf <664158315@qq.com>
2016-10-16 16:23:44 +0800

    解决冲突

--- MSOHCode/Client/src/util/ResultMessage.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-
-public enum ResultMessage {
-		Login_Success, Login_Blank, Login_NoUser, Login_WrongPassword; 
-
-}
--- MSOHCode/Server/src/util/ResultMessage.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-
-public class ResultMessage {
-
-public enum ResultMessage {
-	Login_Success, Login_Blank, Login_NoUser, Login_WrongPassword;
-
-}
