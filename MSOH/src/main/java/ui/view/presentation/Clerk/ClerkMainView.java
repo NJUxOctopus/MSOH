7293acff7873374becd7ef81d412ad7147ad84da
Sunnnnnnnnny <xiaoxiaosang1997@163.com>
2016-11-10 20:01:00 +0800

    try st

---
+++ MSOH/src/main/java/ui/view/presentation/Clerk/ClerkMainView.java
@@ -0,0 +1,18 @@
+package ui.view.presentation.Clerk;/**
+ * Created by Administrator on 2016/11/10.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClerkMainView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
