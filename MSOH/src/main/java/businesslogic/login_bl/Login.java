78d9ec35c6ab2671ca95d3e8cebe8522843cdbc4
Atlas-zqh <free.hee@hotmail.com>
2016-11-13 21:51:34 +0800

    Login的mockObject

---
+++ MSOH/src/main/java/businesslogic/login_bl/Login.java
@@ -0,0 +1,13 @@
+package businesslogic.login_bl;
+
+import businesslogicservice.login_blservice.Login_BLService;
+import util.ResultMessage;
+
+/**
+ * Created by 名 on 2016/11/13.
+ */
+public class Login implements Login_BLService {
+    public ResultMessage login(String ID, String password) {
+        return null;
+    }
+}
---
+++ MSOH/src/main/java/businesslogic/login_bl/MockLogin.java
@@ -0,0 +1,22 @@
+package businesslogic.login_bl;
+
+/**
+ * Created by 名 on 2016/11/13.
+ */
+public class MockLogin extends Login{
+    String ID;
+    String password;
+
+    public MockLogin(String ID,String password) {
+        this.ID = ID;
+        this.password=password;
+    }
+
+    public boolean login(){
+        if(ID=="123456"&&password=="123456")
+            return true;
+        else
+            return false;
+    }
+
+}
