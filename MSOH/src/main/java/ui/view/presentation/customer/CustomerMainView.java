9d40164ea5840eb1f8045aba0f2cfd2c167b75ca
island <549378813@qq.com>
2016-11-17 21:00:58 +0800

    客户除酒店相关界面

---
+++ MSOH/src/main/java/ui/view/presentation/customer/CustomerMainView.java
@@ -0,0 +1,18 @@
+package ui.view.presentation.customer;/**
+ * Created by qky on 2016/11/14.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CustomerMainView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
