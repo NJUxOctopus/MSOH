97a8e0d7cc749bbbfe2742057ca4681c60d638f9
island <549378813@qq.com>
2016-11-18 15:52:08 +0800

    完成客户模块除酒店详细信息界面外的界面

---
+++ MSOH/src/main/java/ui/view/presentation/customer/CustomerMyHotelView.java
@@ -0,0 +1,18 @@
+package ui.view.presentation.customer;/**
+ * Created by island on 2016/11/18.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CustomerMyHotelView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
