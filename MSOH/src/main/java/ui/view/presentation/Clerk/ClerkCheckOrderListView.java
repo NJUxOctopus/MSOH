e12c377dd11e18029e6d44e9c04379aa29beee41
Sunnnnnnnnny <xiaoxiaosang1997@163.com>
2016-11-19 21:01:16 +0800

    Login和Clerk的一部分

---
+++ MSOH/src/main/java/ui/view/presentation/Clerk/ClerkCheckOrderListView.java
@@ -0,0 +1,19 @@
+package ui.view.presentation.Clerk;
+/**
+ * Created by Administrator on 2016/11/15.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClerkCheckOrderListView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
