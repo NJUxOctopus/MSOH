ceeb75cd467c5d378da6dab1a397a71914e115e9
Cflelf <664158315@qq.com>
2016-11-21 19:51:54 +0800

    memberlevel

---
+++ MSOH/src/main/java/dataservice/memberlevel_dataservice/MemberLevel_DataService_Stub.java
@@ -0,0 +1,26 @@
+package dataservice.memberlevel_dataservice;
+
+import po.MemberLevelPO;
+
+import java.rmi.RemoteException;
+
+/**
+ * Created by Pxr on 16/11/19.
+ */
+public class MemberLevel_DataService_Stub implements MemberLevel_DataService {
+    public boolean addMemberLevel(MemberLevelPO memberLevelPO) throws RemoteException {
+        return false;
+    }
+
+    public boolean deleteMemberLevel(MemberLevelPO memberLevelPO) throws RemoteException {
+        return false;
+    }
+
+    public boolean updateMemberLevel(MemberLevelPO memberLevelPO) throws RemoteException {
+        return false;
+    }
+
+    public MemberLevelPO getMemberLevel() throws RemoteException {
+        return null;
+    }
+}
