c3d747f7f1080dce1fa6fb1e852755f93a64e4d7
Cflelf <664158315@qq.com>
2016-11-21 19:41:30 +0800

    util bl service

---
+++ MSOH/src/main/java/businesslogicservice/promotionUtil_blservice/PromotionUtil_BLService.java
@@ -0,0 +1,16 @@
+package businesslogicservice.promotionUtil_blservice;
+
+import vo.PromotionVO;
+
+import java.rmi.RemoteException;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by apple on 16/11/10.
+ */
+public interface PromotionUtil_BLService {
+    public List<PromotionVO> getAll (Date date)throws RemoteException;
+
+    public PromotionVO getSingle(String promotionID)throws RemoteException;
+}
