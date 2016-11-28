48cc5ba2a8b79c9c479982efca52ec8c5a824e94
Atlas-zqh <free.hee@hotmail.com>
2016-11-28 22:55:10 +0800

    Server端的测试类

---
+++ MSOHServer/src/test/java/po/CommentPOTest.java
@@ -0,0 +1,23 @@
+package po;
+
+import org.hibernate.Session;
+import org.junit.Test;
+import util.HibernateUtil;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by zqh on 2016/11/23.
+ */
+public class CommentPOTest {
+    @Test
+    public void testSaveCommentPO(){
+        Session session= HibernateUtil.getSession();
+
+        CommentPO c=new CommentPO(55,"bad","st","320581199703032586","hgg","52363523","201611230002",new Timestamp(System.currentTimeMillis()));
+        session.beginTransaction();
+        session.save(c);
+        session.getTransaction().commit();
+        HibernateUtil.closeSession(session);
+    }
+}
---
+++ MSOHServer/src/test/java/po/CreditRecordPOTest.java
@@ -0,0 +1,23 @@
+package po;
+
+import org.hibernate.Session;
+import org.junit.Test;
+import util.HibernateUtil;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by zqh on 2016/11/24.
+ */
+public class CreditRecordPOTest {
+    @Test
+    public void testSaveCreditRecord(){
+        CreditRecordPO cr=new CreditRecordPO(200,new Timestamp(System.currentTimeMillis()),"zqh","320581199707230236",3200,"201611240001","pxr");
+
+        Session session= HibernateUtil.getSession();
+        session.beginTransaction();
+        session.save(cr);
+        session.getTransaction().commit();
+        HibernateUtil.closeSession(session);
+    }
+}
