6499121f33450e49f31a2ee9f11d69eba7ddb536
Sunnnnnnnnny <xiaoxiaosang1997@163.com>
2016-12-03 16:28:46 +0800

    上一次commit遗漏的ErrorBoxController

---
+++ MSOH/src/main/java/ui/view/presentation/util/ErrorBoxController.java
@@ -0,0 +1,42 @@
+package ui.view.presentation.util;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import ui.view.presentation.StageController;
+import util.ResultMessage;
+
+/**
+ * Created by ST on 2016/12/2.
+ */
+public class ErrorBoxController implements ControlledStage {
+
+    private StageController stageController;
+
+    private ResultMessage resultMessage;
+
+    @FXML
+    private Label errorTypeLabel;
+
+    @Override
+    public void setStageController(StageController stageController) {
+        this.stageController = stageController;
+    }
+
+    public void setErrorType(ResultMessage theResultMessage) {
+        this.resultMessage = theResultMessage;
+    }
+
+    public void setLabel(String error) {
+        errorTypeLabel.setText(error);
+    }
+
+    /**
+     * 取消按钮结果，关闭弹窗
+     */
+    @FXML
+    private void cancel() {
+        stageController = new StageController();
+        stageController.closeStage("util/ErrorBoxView.fxml");
+    }
+
+}
