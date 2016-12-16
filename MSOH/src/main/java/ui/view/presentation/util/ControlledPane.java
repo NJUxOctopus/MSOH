890a65f415cb06285282bc010c440551955b2a94
Atlas-zqh <free.hee@hotmail.com>
2016-12-16 10:36:08 +0800

    新增ControlledPane

---
+++ MSOH/src/main/java/ui/view/presentation/util/ControlledPane.java
@@ -0,0 +1,12 @@
+package ui.view.presentation.util;
+
+import ui.view.presentation.PaneAdder;
+import ui.view.presentation.StageController;
+
+/**
+ * Created by qky on 2016/12/15.
+ */
+public interface ControlledPane {
+    public void setPaneController(PaneAdder paneAdder);
+
+}
