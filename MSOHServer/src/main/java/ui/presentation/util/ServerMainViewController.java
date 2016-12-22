f597ad309b8b66780113b504b214f4a7b153a93e
island <549378813@qq.com>
2016-12-22 19:57:22 +0800

    更改服务器界面

--- MSOHServer/src/main/java/ui/presentation/util/ServerMainViewController.java
+++ MSOHServer/src/main/java/ui/presentation/util/ServerMainViewController.java
@@ -1,34 +1,15 @@
 package ui.presentation.util;
 
-import init.LoopedStreams;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Scene;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import javafx.stage.Modality;
-import javafx.stage.Screen;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
-import javax.swing.*;
-import javax.swing.text.Document;
-import java.io.*;
 
 /**
  * Created by island on 2016/12/21.
  */
 public class ServerMainViewController {
-    private String resource = "ServerMainView.fxml";
-
-    private Stage stage;
-
-    private FXMLLoader loader;
-
-    private String str = "";
 
     @FXML
     private TextField ipTextField;
@@ -41,7 +22,6 @@ public class ServerMainViewController {
 
     public void setIP(String IP){
         ipTextField.setText(IP);
-        startConsoleReaderThread();
     }
 
 
@@ -50,34 +30,4 @@ public class ServerMainViewController {
         System.exit(0);
     }
 
-    private void startConsoleReaderThread() {
-        try {
-            final LoopedStreams ls = new LoopedStreams();
-            PrintStream ps = new PrintStream(ls.getOutputStream());
-            System.setOut(ps);
-            System.setErr(ps);
-            InputStream inStream = ls.getInputStream();
-
-
-            final BufferedReader br =
-                    new BufferedReader(new InputStreamReader(inStream));
-            new Thread(new Runnable() {
-                public void run() {
-                    StringBuffer sb = new StringBuffer();
-                    try {
-                        String s;
-                        while ((s = br.readLine()) != null) {
-                            str += '\n';
-                            str += s;
-                            textArea.setText(str);
-                        }
-                    } catch (IOException e) {
-                        System.exit(1);
-                    }
-                }
-            }).start();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-    } // startConsoleReaderThread()
 }
